c9ef238ba42939f6c7beeccb827cba5519635656
Clément <clemichoux@gmail.com>
2022-02-19 19:35:03 +0100

    update

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -7,7 +7,7 @@ const multer = require('../middleware/multer-config');
 
 router.post('/',auth, multer, sauceCtrl.createSauce);
 router.put('/:id',auth, multer, sauceCtrl.modifySauce);
-router.delete('/:id',auth, sauceCtrl.deleteSauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
 router.get('/:id',auth, sauceCtrl.getOneSauce);
 router.get('/',auth, sauceCtrl.getAllSauces);
 router.post('/:id/like', auth, sauceCtrl.postLike);
